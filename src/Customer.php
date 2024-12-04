611bfa78881039875ca9e63267a647babd1d8868
naingaunglinn <naingaunglinn369@gmail.com>
2024-12-05 05:22:51 +0630

    update: null safe operator

---
+++ src/Customer.php
@@ -0,0 +1,14 @@
+<?php 
+
+class Customer
+{
+    private ?PaymentProfile $paymentProfile = null;
+
+    /**
+     * @return PaymentProfile|null
+    */
+    public function getPaymentProfile(): ?PaymentProfile
+    {
+        return $this->paymentProfile;
+    }
+}
--- src/Transaction.php
+++ src/Transaction.php
@@ -2,37 +2,14 @@
 declare(strict_types= 1);
 
 class Transaction
-{
-    private float $amount;
-    private string $description;
-
-    public function __construct(float $amount, string $description)
-    {
-        $this->amount = $amount;
-        $this->description = $description;
+{    
+    private ?Customer $customer = null;
+    public function __construct(
+        private float $amount, 
+        private string $description
+    ){
+        
     }
     
-    public function addTax(float $rate): Transaction
-    {
-        $this->amount += $this->amount * $rate / 100;
-
-        return $this;
-    }
-
-    public function applyDsicount(float $rate): Transaction
-    {
-        $this->amount -= $this->amount * $rate /100;
-
-        return $this;
-    }
-
-    public function getAmount()
-    {
-        return $this->amount;
-    }
-
-    public function __destruct()
-    {
-        echo "Desturct " . $this->description . '<br />';
-    }
+    
 }
